a03fe5b058c79506c601d507986afbfc665ed63c
Roy Sommer <royso@wix.com>
2022-07-03 20:51:24 +0300

    support gh-pages when building

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: process.env.GH_PAGES_DEPLOY === 'true' ? '/ripple/' : '/',
   resolve: {
     alias: {
-      'jimp': 'jimp/browser/lib/jimp'
-    }
-  }
-})
+      jimp: 'jimp/browser/lib/jimp',
+    },
+  },
+});
